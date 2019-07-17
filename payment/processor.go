e2f50fb6d408f4cd7b9bdc91be873ef2794e7eaf
BrunoDM2943 <bdm2943@gmail.com>
2019-07-16 22:04:28 -0300

    Adding new example of processor for benchmark

--- payment/processor.go
+++ payment/processor.go
@@ -1,57 +1,26 @@
 package payment
 
 import (
-	"sync"
-	"time"
+	"encoding/json"
+	"os"
 
 	"github.com/BrunoDM2943/pock_bank-api/domain"
 )
 
-type paymentProcessor struct {
-	database map[int64][]*domain.Payment
-}
-
-func NewPaymentProcessor() paymentProcessor {
-	return paymentProcessor{
-		database: make(map[int64][]*domain.Payment),
-	}
-}
-
 func processPayments(payments []domain.Payment) error {
-	for range payments {
-		time.Sleep(100 * time.Millisecond)
+	file, _ := os.Create("/tmp/paymentsPointer")
+	for _, payment := range payments {
+		b, _ := json.Marshal(payment)
+		file.Write(b)
 	}
 	return nil
 }
 
-func processPaymentsFaster(payments []domain.Payment) error {
-	for range payments {
-		time.Sleep(50 * time.Millisecond)
+func processPaymentsFaster(payments []*domain.Payment) error {
+	file, _ := os.Create("/tmp/paymentsValue")
+	for _, payment := range payments {
+		b, _ := json.Marshal(payment)
+		file.Write(b)
 	}
 	return nil
-}
-
-func processPaymentsParallel(payments []domain.Payment) error {
-	wg := sync.WaitGroup{}
-	for range payments {
-		wg.Add(1)
-		go func() {
-			time.Sleep(100 * time.Millisecond)
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	return nil
-}
-
-func (this paymentProcessor) pay(paymentId int, value float32, user *domain.Account) {
-	payment := &domain.Payment{
-		ID:     paymentId,
-		Value:  value,
-		Date:   time.Now(),
-		UserID: user.ID,
-	}
-	payments := append(this.database[user.ID], payment)
-	this.database[user.ID] = payments
-
 }
--- payment/processor_test.go
+++ payment/processor_test.go
@@ -8,38 +8,31 @@ import (
 
 func BenchmarkProcessPayments(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		processPayments(mockList(100))
+		processPayments(mockList(1000))
 	}
 }
 
 func BenchmarkProcessPaymentsFaster(b *testing.B) {
 	for n := 0; n < b.N; n++ {
-		processPaymentsFaster(mockList(100))
+		processPaymentsFaster(mockListPointer(1000))
 	}
-}
-
-func BenchmarkProcessPaymentsParalel(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		processPaymentsParallel(mockList(100))
-	}
-}
-
-func BenchmarkPayInParallel(b *testing.B) {
-	processor := NewPaymentProcessor()
-	user := &domain.Account{
-		ID: 19,
-	}
-	b.RunParallel(func(pb *testing.PB) {
-		for pb.Next() {
-			processor.pay(1, 1, user)
-		}
-	})
 }
 
 func mockList(qtd int) []domain.Payment {
 	payments := make([]domain.Payment, 0)
 	for i := 0; i < qtd; i++ {
 		p := domain.Payment{
+			ID: i,
+		}
+		payments = append(payments, p)
+	}
+	return payments
+}
+
+func mockListPointer(qtd int) []*domain.Payment {
+	payments := make([]*domain.Payment, 0)
+	for i := 0; i < qtd; i++ {
+		p := &domain.Payment{
 			ID: i,
 		}
 		payments = append(payments, p)
